ab1ba9b4b78d49d0fe67253832c5645910c75853
kukurudz-nadia <kukurudznadia6@gmail.com>
2025-02-20 19:48:42 +0200

    Create a player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import  Player from './components/Player';
 
 function App() {
   return (
-  <p>comming soon..</p>
+    <main>
+      <div id='game-container'>
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        BOARD
+      </div>
+      logs
+    </main>
   )
 }
 
