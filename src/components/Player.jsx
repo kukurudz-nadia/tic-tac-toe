3904440ad15389df5d64c9ec7dc271742e304e6d
kukurudz-nadia <kukurudznadia6@gmail.com>
2025-02-21 18:22:41 +0200

    Add input logic

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 
 export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEditClick = () => {
     setIsEditing((editing) => !editing);
   };
 
+  const handleChangeName = (event) => {
+    setPlayerName(event.target.value);
+  };
+
   return (
     <li>
       <span className="player">
         {isEditing
-          ? <input type='text' value={name} required></input>
-          : <span className="player-name">{name}</span>
+          ? <input type='text' required onChange={handleChangeName}></input>
+          : <span className="player-name">{playerName}</span>
         }
         <span className="player-symbol">{symbol}</span>
       </span>
